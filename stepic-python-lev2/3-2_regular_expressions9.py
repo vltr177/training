ae4330c740e9acff8184c1a9c4186e74c8867195
vltr177 <82467192+vltr177@users.noreply.github.com>
2024-04-25 19:28:08 +0300

    Create 3-2_regular_expressions9.py

---
+++ stepic-python-lev2/3-2_regular_expressions9.py
@@ -0,0 +1,16 @@
+# Вам дана последовательность строк.
+# В каждой строке замените все вхождения нескольких одинаковых букв на одну букву.
+# Буквой считается символ из группы \w.
+# Sample Input:
+# attraction
+# buzzzz
+# Sample Output:
+# atraction
+# buz
+
+import sys
+import re
+
+for line in sys.stdin:
+  line = line.rstrip ()
+  print (re.sub(r'(\w)\1+', r'\1', line))
