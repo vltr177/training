7d2d86812b539c46ce0992f42b5c74bc4d6a2090
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-01 18:59:24 +0300

    Create 2-2_JAVA_Basics_2.java

---
+++ java_basics/2-2_JAVA_Basics_2.java
@@ -0,0 +1,12 @@
+/ Создайте две переменные: целого типа a и действительного b, равные 5 и 0.4 соответственно. Выведите построчно на экран их сумму, разность, произведение и частное.
+
+class MyProgram{
+    public static void main(String[] args){
+    int a = 5;
+    float b = 0.4f;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    }
+}
