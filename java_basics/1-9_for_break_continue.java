6a30fc6b63b0284d335829a06be8bd2ca4853515
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-07 22:58:27 +0300

    Update 1-9_for_break_continue.java

--- java_basics/1-9_for_break_continue.java
+++ java_basics/1-9_for_break_continue.java
@@ -162,3 +162,19 @@ class MyProgram {
     }
 }
     
+// Геометрическая прогрессия
+// По данному целому числу a и натуральному n вычислите сумму 1+a+a2+...+an, не используя формулу суммы геометрической прогрессии. Время работы программы должно быть пропорционально n.
+import java.util.Scanner;
+
+class MyProgram {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 1;
+        for ( int i = 1; i <= n ; i++ ){
+            sum += Math.pow(a,i);
+        }
+        System.out.println(sum);
+    }
+}
