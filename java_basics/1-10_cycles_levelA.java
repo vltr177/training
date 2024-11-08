ac10cf4b5a7b27049a07d80faa00beeadd6e9b64
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-08 18:46:02 +0300

    Update 1-10_cycles_levelA.java

--- java_basics/1-10_cycles_levelA.java
+++ java_basics/1-10_cycles_levelA.java
@@ -10,3 +10,35 @@ class MyProgram{
     }
 }
 
+// Сумма чисел
+// Напишите программу, которая находит сумму натуральных чисел от 1 до n, где n вводится с клавиатуры.
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum += i;
+            }
+        System.out.println(sum);
+    }
+}
+
+// Сумма квадратов
+// Напишите программу, которая находит сумму квадратов натуральных чисел от 1 до n, где n вводится с клавиатуры.
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int power = 0;
+        for (int i = 1; i <= n; i++){
+             power += Math.pow(i,2);
+            }
+        System.out.println(power);
+    }
+}
+
