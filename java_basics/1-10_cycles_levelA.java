3738fbd0c187b1d1c36ad7683f52ee1f456ff6fc
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-08 18:36:21 +0300

    Create 1-10_cycles_levelA.java

---
+++ java_basics/1-10_cycles_levelA.java
@@ -0,0 +1,12 @@
+// Повтори 10 раз!
+// Напишите программу, которая выводит на экран слово "Привет" (без кавычек) 10 раз.
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        for ( int i = 0; i < 10; i++){
+            System.out.println("Привет");
+        }
+    }
+}
+
