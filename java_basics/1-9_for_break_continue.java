6d3865c0ea3e992535c6afa2bc71032784f48cbf
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-08 05:39:14 +0300

    Update 1-9_for_break_continue.java

--- java_basics/1-9_for_break_continue.java
+++ java_basics/1-9_for_break_continue.java
@@ -178,3 +178,19 @@ class MyProgram {
         System.out.println(sum);
     }
 }
+
+// Двоичная запись
+// Дано натуральное число N. Выведите его представление в двоичном виде в обратном порядке.
+import java.util.Scanner;
+
+class MyProgram {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int res = 0;
+        for ( int i = N; i != 0 ; i /= 2 ){
+            res = i % 2;
+            System.out.print(res);
+        }
+    }
+}
