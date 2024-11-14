7774e57d672c69fdfc332416e43b25bf2537c271
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-14 23:09:34 +0300

    Update test.java

--- java_basics/test.java
+++ java_basics/test.java
@@ -1,114 +1,46 @@
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class choice_search {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int res = -1;                                           // переменная с результатом поиска
+        int res = -1;                                                   // переменная с результатом поиска
 
         // Создаём массив и наполняем случайными числами
-        int len = 100;                                          // размер массива
+        int len = 100000000;                                                  // размер массива
         Random rnd = new Random();
-        int[] numbers = new int[len];                           // инициализируем массив с целыми числами заданного размера
-        for (int i = 0; i < len; i++) {                         // заполняем массив случайными значениями от 0 до 1000000
-            numbers[i] = rnd.nextInt(100);
+        int[] numbers = new int[len];                                   // инициализируем массив с целыми числами заданного размера
+        for (int i = 0; i < len; i++) {                                 // заполняем массив случайными значениями от 0 до 1000000
+            numbers[i] = rnd.nextInt(1000);
         }
 
-        // Поиск
-        int value = sc.nextInt();                               // запрашиваем искомый элемент
-        int choise = sc.nextInt();                              // запрашиваем метод поиска (1 - простой, !1 - бинарный)
-        if (choise == 1) {
-            res = search1(numbers, value, 0, len - 1);                      // вызываем функцию простого поиска
-            }
-        else {
-            // сортируем массив
-            qSort(numbers, 0, 99);
-            for (int i = 0; i < len; i++)
-                System.out.println(numbers[i]);
-            // ищем бинарным поиском
-            res = search2(numbers, value, 0, len - 1);
-        }
-        if (res != -1) {                                        // выводим результат
-            System.out.println("Элемент найден в позиции " + res);
-        } else {
-            System.out.println("Элемент не найден");
-        }
+        int value = sc.nextInt();                                       // запрашиваем искомый элемент
+
+        // Поиск с перебором значений
+        System.out.println("Поиск с использованием перебора значений");
+        long start = System.currentTimeMillis();
+        res = search1(numbers, value);                                  // вызываем функцию простого поиска
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        print(res, elapsed);
+        res = -1;
+
+        // Бинарный поиск
+        System.out.println("Бинарный поиск");
+        start = System.currentTimeMillis();
+        qSort(numbers, 0, len - 1);
+        res = search2(numbers, value, 0, len - 1);            // вызываем функцию бинарного поиска
+        finish = System.currentTimeMillis();
+        elapsed = finish - start;
+        print(res, elapsed);
     }
 
-    // Функция простого поиска
+
+    // Функция "Поиск перебором значений"
     public static int search1(int[] numbers, int value) {
         for (int i = 0; i < numbers.length; i++) {
             if (value == numbers[i])
                 return i;
         }
         return -1;
-    }
-
-
-    // Функция быстрой сортировки массива
-    public static void qSort(int[] array, int begin, int end) {
-        if (end <= begin) return;
-        int pivot = partition(array, begin, end);
-        qSort(array, begin, pivot-1);
-        qSort(array, pivot+1, end);
-    }
-    static int partition(int[] array, int begin, int end) {
-        int pivot = end;
-
-        int counter = begin;
-        for (int i = begin; i < end; i++) {
-            if (array[i] < array[pivot]) {
-                int temp = array[counter];
-                array[counter] = array[i];
-                array[i] = temp;
-                counter++;
-            }
-        }
-        int temp = array[pivot];
-        array[pivot] = array[counter];
-        array[counter] = temp;
-
-        return counter;
-    }
-
-
-
-    // Функция бинарного поиска
-
-    public static int search2(int[] numbers, int value, int start, int end) {
-        int label = end/2;
-        if (value < label) {
-            start = start;
-            end = label}
-        }
-        if (value > label) {
-             start = label;
-             end = end;
-        }
-        else
-            return value;
-        search2(int[numbers, value, start, end);
-    }
-
-
-    // Функция измерения времени
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
