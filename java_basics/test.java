6c5c1a1e642d2771c409fc0485e8bfb3e4f88293
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-15 21:23:26 +0300

    Update test.java

--- java_basics/test.java
+++ java_basics/test.java
@@ -1,54 +1,55 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
 public class choice_search {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int res = -1;                                                   // переменная с результатом поиска
+        boolean flag = false;                           // флаг успеха поиска
 
-        // Создаём массив и наполняем случайными числами
-        int len = 10000000;                                                  // размер массива
+        // ГЕНЕРАЦИЯ МАССИВА СЛУЧАЙНЫХ ЧИСЕЛ
+        int len = 100000;                               // размер массива 100 000
         Random rnd = new Random();
-        int[] numbers = new int[len];                                   // инициализируем массив с целыми числами заданного размера
-        for (int i = 0; i < len; i++) {                                 // заполняем массив случайными значениями от 0 до 1000000
-            numbers[i] = rnd.nextInt(10000000);
+        int[] numbers = new int[len];                   // инициализируем массив с целыми числами заданного размера
+        for (int i = 0; i < len; i++) {                 // заполняем массив случайными значениями от 0 до 100 000
+            numbers[i] = rnd.nextInt(100000);
         }
+        int value = sc.nextInt();                       // запрашиваем искомый элемент
 
-        int value = sc.nextInt();                                       // запрашиваем искомый элемент
-
-        // Поиск с перебором значений
+        // ПРОСТОЙ ПОИСК O(N)
         System.out.println("ПРОСТОЙ ПОИСК");
-        long start_time = System.nanoTime();
-        res = search1(numbers, value);                                  // вызываем функцию простого поиска
-        result(res);
+        long start_time = System.nanoTime();            // фиксируем запуск таймера
+        search1(numbers, value, flag);                  // вызов функции простого поиска
+        result(flag);                                   // вызов результата простого поиска
         long end_time = System.nanoTime();
+        // вывод времени поиска, нс/1 000 000 в мс
         System.out.println("Время простого поиска = " + ((end_time - start_time)/1000000) + " мс");
 
-        res = -1;
+        flag = false;
 
-        // Бинарный поиск
+        // БЫСТРАЯ СОРТИРОВКА МАСИВА O(N * log N) || (O * N^2) + БИНАРНЫЙ ПОИСК O(log N)
         System.out.println("БЫСТРАЯ СОРТИРОВКА МАССИВА");
         start_time = System.nanoTime();
-        qSort(numbers, 0, len - 1);
+        qSort(numbers, 0, len - 1);          // вызов функции быстрой сортировки
         end_time = System.nanoTime();
         System.out.println("Время сортировки = " +   ((end_time - start_time)/1000000) + " мс");
         System.out.println("БИНАРНЫЙ ПОИСК");
         start_time = System.nanoTime();
-        res = search2(numbers, value, 0, len - 1);            // вызываем функцию бинарного поиска
-        result(res);
+        // вызов функции бинарного поиска
+        search2(numbers, value, 0, len - 1, flag);
+        result(flag);
         end_time = System.nanoTime();
         System.out.println("Время бинарного поиска = " +  ((end_time - start_time)/1000000) + " мс");
     }
 
-
     // Функция "Поиск перебором значений"
-    public static int search1(int[] numbers, int value) {
+    public static void search1(int[] numbers, int value, boolean flag) {
         for (int i = 0; i < numbers.length; i++) {
-            if (value == numbers[i])
-                return i;
+            if (value == numbers[i]) {
+                flag = true;
+                return;
+            }
         }
-        return -1;
+        return;
     }
 
     // ФУНКЦИЯ "Быстрая сортировка массива"
@@ -61,7 +62,6 @@ public class choice_search {
     // ФУНКЦИЯ "Перераспределение массива для быстрой сортировки"
     static int partition(int[] array, int begin, int end) {
         int pivot = array[begin + (end - begin) / 2];
-
         int counter = begin;
         for (int i = begin; i < end; i++) {
             if (array[i] < array[pivot]) {
@@ -74,28 +74,30 @@ public class choice_search {
         int temp = array[pivot];
         array[pivot] = array[counter];
         array[counter] = temp;
-
         return counter;
     }
 
     // ФУНКЦИЯ "Бинарный поиск"
-    public static int search2(int[] numbers, int value, int low, int high) {
+    public static void search2(int[] numbers, int value, int low, int high, boolean flag) {
         while (low <= high) {
             int mid = (low + high) / 2;
             int label = numbers[mid];
-            if (value == label)
-                return mid;
+            if (value == label) {
+                flag = true;
+                return;
+            }
             if (value < label)
                 high = mid - 1;
             if (value > label)
                 low = mid + 1;
         }
-        return -1;
+        return;
     }
 
-    public static void result(int res) {
-        if (res != -1) {                                                // выводим результат
-            System.out.println("Элемент найден в позиции " + res);
+    // ФУНКЦИЯ "Вывод результата"
+    public static void result(boolean flag) {
+        if (flag == false) {
+            System.out.println("Элемент найден");
         } else {
             System.out.println("Элемент не найден");
         }
