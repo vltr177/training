7619cb98802a0572a1e671d9a67654b39ea4d257
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-28 11:41:35 +0300

    Update java_basics

---
+++ java_basics/1_14.java
@@ -0,0 +1,19 @@
+//1
+
+import java.util.Scanner;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float[] array1 = new float[n];
+        float[] array2 = new float[n];
+        for (int i = 0; i < n; i++)
+            array1[i] = sc.nextFloat();
+        for (int i = 0; i < n; i++)
+            array2[i] = sc.nextFloat();
+        for (int i = 0; i < n; i++)
+            System.out.println(array1[i] + array2[i]);
+    }
+}
+
+//2
