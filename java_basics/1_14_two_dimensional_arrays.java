a24ff8fa3d3ea579e3dff4778ad39ac123d3ef09
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-11-29 12:17:57 +0300

    Update 1_14_two_dimensional_arrays.java

--- java_basics/1_14_two_dimensional_arrays.java
+++ java_basics/1_14_two_dimensional_arrays.java
@@ -196,3 +196,31 @@ class MyProgram{
             System.out.print(array[sum_max_row][j] + " ");
     }
 }
+
+//
+
+import java.util.Scanner;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] array = new int[n][m];
+
+        for (int i = 0; i<n; i++)
+            for (int j = 0; j<m; j++)
+                array[i][j] = sc.nextInt();
+
+        int[][] array2 = new int[m][n];    
+        for (int i = 0; i<m; i++)
+            for (int j = 0; j<n; j++)
+                array2[i][j] = array[n-j-1][i];
+        
+        for (int i = 0; i<m; i++){
+            for (int j = 0; j<n; j++){
+                System.out.print(array2[i][j] + " ");
+            }
+        System.out.println();
+        }
+    }
+}
