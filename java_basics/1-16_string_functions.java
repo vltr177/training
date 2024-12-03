e2c75464a77991adbe42f64dc0253e67b4320a01
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-03 15:18:21 +0300

    Update 1-16_string_functions.java

--- java_basics/1-16_string_functions.java
+++ java_basics/1-16_string_functions.java
@@ -51,3 +51,20 @@ class MyProgram{
     }
 }
 
+4----
+
+import java.util.Scanner;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] words = new String[n];
+        for (int i=0;i<n;i++)
+            words[i] = sc.next();
+        String splitter = sc.next();
+        String ans = String.join(splitter, words);
+        System.out.println(ans);
+    }
+}
+
+
