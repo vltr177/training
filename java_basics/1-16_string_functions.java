67080eeb10d476f44896b981efd1c7a25d0830b7
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-03 14:26:42 +0300

    Update 1-16_string_functions.java

--- java_basics/1-16_string_functions.java
+++ java_basics/1-16_string_functions.java
@@ -35,3 +35,19 @@ class MyProgram{
         System.out.println(len);
     }
 }
+
+3----
+
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.nextLine();
+        String word2 = sc.nextLine();
+        String text = sc.nextLine();
+        String new_text = text.replace(word1, word2);
+        System.out.println(new_text);
+    }
+}
+
