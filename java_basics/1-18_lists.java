83118936c1992a8cba91339d487bb38a29d4c216
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-05 21:34:11 +0300

    Update 1-18_lists.java

--- java_basics/1-18_lists.java
+++ java_basics/1-18_lists.java
@@ -178,4 +178,37 @@ class MyProgram{
     }
 }
 
+/*Стек
+Напишите программу, которая реализует структуру данных стек, используя список.
+Условие задачи таково: мальчик Петя использует свой стул в качестве многоместной вешалки. А именно: накидывает на него вещи. Иногда он забирает верхнюю вещь. Нужно вывести на экран порядок его вещей от спинку стула.
+Сначала водится число n - количество запросов в вашей программе. Каждый из запросов будет иметь один следующих видов:
+push <одежда> - Петя вешает на стул элемент одежды
+pop - снимает одежду со стула. При этом на экран выводится запись вида "Петя взял <одежда>"
+После выполнения всех запросов выведите оставшиеся на стуле вещи. Гарантируется, что название вещи состоит из одного слова. */
+import java.util.Scanner;
+import java.util.ArrayList;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> wear = new ArrayList<>();
+        int n = sc.nextInt();
+        sc.nextLine();
+        String cmd = "";
+        String name = "";
+        for (int i = 0; i<n; i++){
+            cmd = sc.nextLine();
+            if (cmd.contains("push")){
+                name = cmd.replace("push ","");
+                wear.add(name);                
+            }
+            if (cmd.contains("pop")){
+                System.out.println("Петя взял " + wear.get(wear.size() - 1));
+                wear.remove(wear.size() - 1);
+            }
+        }
+        
+        for (String thing : wear)
+            System.out.print(thing + " ");
+    }
+}
 
