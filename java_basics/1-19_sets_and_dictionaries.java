bbd704c75af2c6af7b42e19fcc4dd9fe18b40aa5
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-06 19:28:23 +0300

    Update 1-19_sets_and_dictionaries.java

--- java_basics/1-19_sets_and_dictionaries.java
+++ java_basics/1-19_sets_and_dictionaries.java
@@ -4,3 +4,24 @@
 На первой строчке вводится число n, на второй число k. Затем вводится k пар чисел. В каждой паре первое число - объект, который надо вставить в список, а второе - индекс, куда его нужно вставить, гарантируется, что все индексы корректны.  
 Выходные данные:
 Выведите полученный список на экран через пробел.*/
+import java.util.Scanner;
+import java.util.ArrayList;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int a = 0;
+        int b = 0;
+        for (int i = 1; i<=n; i++)
+            list.add(i);
+        for (int i = 0; i<k; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            list.add(b, a);
+        }
+        for (Integer cell : list)
+            System.out.print(cell + " ");
+    }
+}
