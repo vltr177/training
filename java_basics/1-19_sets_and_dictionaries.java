4cb473e1909d10450ac944d0292fe472f9331454
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-07 22:34:03 +0300

    Update 1-19_sets_and_dictionaries.java

--- java_basics/1-19_sets_and_dictionaries.java
+++ java_basics/1-19_sets_and_dictionaries.java
@@ -77,3 +77,42 @@ class MyProgram{
         System.out.println(ans);
     }
 }
+
+// Статистика по Персоналу
+// Вводится число n. Затем 2*n строк. Каждая пара строк - имя человека и его профессия. Определите, сотрудников какой профессии больше всего. Выведите на экран это количество, профессию и их имена на экран в том же порядке, в котором они вводились. Гарантируется, что будет введено не более 100 сотрудников.
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Scanner;
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        LinkedHashMap<String,String>workers=new LinkedHashMap<String,String>();
+        HashMap<String, Integer> profession = new HashMap<>();
+        String ans = "";
+        String search = "";
+        int max = 0;
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i=0; i<n; i++)
+            workers.put(sc.next(), sc.next());
+        for (String key: workers.keySet()){
+            search = workers.get(key);
+            if (profession.containsKey(search))
+                profession.replace(search,profession.get(search)+1);
+            else
+                profession.put(search,1);
+        }
+        for (String key: profession.keySet()){
+            if (profession.get(key)>max){
+                max = profession.get(key);
+                ans = key;
+            }
+        }
+        System.out.println(max);
+        System.out.println(ans);
+        for (String key: workers.keySet())
+           if (workers.get(key).equals(ans))
+               System.out.println(key);
+    }
+}
+
