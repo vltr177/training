c8ccab50538c80d4cdebd25b7f48ef01790d3b37
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-15 19:58:37 +0300

    Create 2-01_functions_parameters.java

---
+++ java_basics/2-01_functions_parameters.java
@@ -0,0 +1,20 @@
+// Две функции
+// Допишите функцию my_first_function, которая выводит на экран три строчки "Hello world!". Напишите так же функцию my_second_function, которая выведет на экран одну строку: "Java the best!". Содержание функции main менять запрещено.
+class Example{
+    public static void main(String[] args){
+        my_first_function();
+        my_second_function();
+        my_second_function();
+    }
+
+    static void my_first_function(){
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+
+    static void my_second_function(){
+        System.out.println("Java the best!");
+    }
+}
+
