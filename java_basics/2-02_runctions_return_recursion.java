3f188242b3fff3308e1275cf12bd0c10c89ae9ca
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-18 22:18:03 +0300

    Update 2-02_runctions_return_recursion.java

--- java_basics/2-02_runctions_return_recursion.java
+++ java_basics/2-02_runctions_return_recursion.java
@@ -1,2 +1,15 @@
 // Площадь поверхности параллелепипеда
 // Напишите функцию area, которая принимает на вход 3 натуральных числа - ширину, высоту и длину прямоугольного параллелепипеда, а находит и выводит на экран площадь его поверхности. Содержание функции main менять запрещено.
+import java.util.Scanner;
+
+class Programm{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        area(a, b, c);
+    }
+
+}
