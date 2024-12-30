b3be101f6444f6a2a6b108a4cacf4f4f1071e30b
Vadim Barchuk <vadim.barchuk@gmail.com>
2024-12-30 14:18:35 +0300

    Update 2-05_class_inheritance.java

--- java_basics/2-05_class_inheritance.java
+++ java_basics/2-05_class_inheritance.java
@@ -123,3 +123,142 @@ class Main{
     }
 }
 
+/*Животные наследники
+Создайте три класса - наследника от класса Animal (из предыдущего урока): Bird, Fish и Insect.
+
+Класс Bird:
+По умолчанию будем считать, что все птицы умеют летать.
+У птиц должны быть новые свойства: 
+area - зона обитания
+winterFly - ответ на вопрос, улетает ли она зимовать
+Добавьте птицам метод chirik_chirik, который выводит на экран строку "Chirik-Chirik".
+
+Класс Fish:
+Все рыбы должны уметь плавать по умолчанию.
+У рыб должны быть новые свойства: 
+squama - тип чешуи
+upStreamSwim - умеет ли плавать против течения
+Добавьте рыбам метод bul_bul, который выводит на экран строку "Bul-bul".
+
+Класс Insect:
+Все насекомые должны уметь ходить по умолчанию.
+У насекомых должны быть новые свойства: 
+wingCount - количество крыльев
+likeJesus - умеет ли ходить по воде
+Добавьте насекомым метод ggggg, который выводит на экран строку "Ggggg". */
+
+public class Bird extends Animal {
+
+    private String area;
+    private Boolean winterFly;
+
+    public Bird(){
+        super.setFly(true);
+    }
+
+    public Bird(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, String area, boolean winterFly){
+        super(type, name, age, weight, isFly,isWalk, isSwim);
+        this.area = area;
+        this.winterFly = winterFly;
+        super.setFly(true);
+    }
+
+    public void setArea(String area){ this.area = area; }
+    public void setWinterFly(Boolean winterFly){ this.winterFly = winterFly; }
+    public String getArea(){
+        return area;
+    }
+    public Boolean isWinterFly(){  return winterFly; }
+
+    public void chirick_chirick(){
+        System.out.println("Chirik-Chirik");
+    }
+}
+public class Fish extends Animal {
+
+    private String squama;
+    private Boolean upStreamSwim;
+
+    public Fish(){
+        super.setSwim(true);
+    }
+
+    public Fish(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, String squama, boolean upStreamSwim){
+        super(type, name, age, weight, isFly,isWalk, isSwim);
+        this.squama = squama;
+        this.upStreamSwim = upStreamSwim;
+        super.setSwim(true);
+    }
+
+    public void setSquama(String squama){ this.squama = squama; }
+    public void setUpStreamSwim(Boolean upStreamSwim){ this.upStreamSwim = upStreamSwim; }
+    public String getSquama(){ return squama; }
+    public Boolean isUpStreamSwim(){  return upStreamSwim; }
+
+    public void bul_bul(){
+        System.out.println("Bul-bul");
+    }
+}
+
+public class Insect extends Animal {
+
+    private int wingCount;
+    private Boolean likeJesus;
+
+    public Insect(){
+        super.setWalk(true);
+    }
+
+    public Insect(String type, String name, int age, double weight, boolean isFly, boolean isWalk, boolean isSwim, int wingCount, boolean likeJesus){
+        super(type, name, age, weight, isFly,isWalk, isSwim);
+        this.wingCount = wingCount;
+        this.likeJesus = likeJesus;
+        super.setWalk(true);
+    }
+
+    public void setWingCount(int wingCount){ this.wingCount = wingCount; }
+    public void setLikeJesus(Boolean likeJesus){ this.likeJesus = likeJesus; }
+    public int getWingCount(){ return wingCount; }
+    public Boolean isLikeJesus(){  return likeJesus; }
+
+    public void ggggg(){
+        System.out.println("Ggggg");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.display();
+
+        Bird b = new Bird();
+        b.setName("Bob");
+        b.display();
+        b.setArea("На югах");
+        b.setWinterFly(false);
+        System.out.println(b.getArea());
+        System.out.println(b.isWinterFly());
+        b.chirick_chirick();
+
+        Fish f = new Fish();
+        f.setName("Сельд");
+        f.display();
+        f.setSquama("Крупная");
+        f.setUpStreamSwim(true);
+        System.out.println(f.getSquama());
+        System.out.println(f.isUpStreamSwim());
+        f.bul_bul();
+
+        Insect i = new Insect();
+        i.setName("Жук Жукыч");
+        i.display();
+        i.setWingCount(4);
+        i.setLikeJesus(true);
+        System.out.println(i.getWingCount());
+        System.out.println(i.isLikeJesus());
+        i.ggggg();
+    }
+}
+
+// Переопределённые животные
+// Переопределите у созданных вами классов наследников (Bird, Fish и Insect) метод display. Перед выводом стандартной информации добавьте в начало вывода фразу "I am <название класса>". Так же добавьте в метод display информацию о новых свойствах этого класса. 
