ff4f7af39e45a1366c90ef853a53feafc50ee046
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-02-23 19:37:23 +0300

    Update 4-3_cycles_while.java

--- java-rush/4-3_cycles_while.java
+++ java-rush/4-3_cycles_while.java
@@ -20,3 +20,32 @@ public class Solution {
         System.out.println(sum);
     }
 }
+
+/* Используя вложенные циклы while (цикл в цикле) выведи на экран незаполненный прямоугольник (его контур) размером 10 (высота) на 20 (ширина) из букв 'Б'.
+Незаполненная часть состоит из пробелов.*/
+
+//Незаполненный прямоугольник
+public class Solution {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 10)
+        {
+            int m = 0;
+            while (m < 20)
+            {
+                if ((n==0) || (n==9))
+                    System.out.print("Б");
+                else
+                {
+                    if ((m==0) || (m==19))
+                        System.out.print("Б");
+                    else
+                        System.out.print(" ");
+                }
+                m++;
+            }
+            System.out.println();
+            n++;
+        }
+    }
+}
