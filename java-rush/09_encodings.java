1360de5410a4d2a0120d9213763c28723229b1cf
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-02-27 22:15:41 +0300

    Create 09_encodings.java

---
+++ java-rush/09_encodings.java
@@ -0,0 +1,125 @@
+/* Публичный статический метод toOctal(int) должен переводить целое число, полученное в качестве входящего параметра, из десятичной системы счисления в восьмеричную. А публичный статический метод toDecimal(int) наоборот — из восьмеричной в десятичную.
+Методы работают только с положительными числами. Если входящий параметр меньше или равен 0, методы возвращают 0.
+Твоя задача — реализовать эти методы.
+
+Один из алгоритмов перевода десятичного числа в восьмеричное следующий:
+i равно 0
+while(десятичное число не равно 0) {
+восьмеричное число = восьмеричное число + (остаток от деления десятичного числа на 8) * на 10 в степени i
+десятичное число = десятичное число / 8
+i увеличиваем на 1
+}
+
+Один из алгоритмов перевода восьмеричного числа в десятичное следующий:
+i равно 0
+while(восьмеричное число не равно 0) {
+десятичное число = десятичное число + (остаток от деления восьмеричного числа на 10) * на 8 в степени i
+восьмеричное число = восьмеричное число / 10
+i увеличиваем на 1
+}
+*/
+
+import java.util.regex.Pattern;
+
+/* 
+Восьмеричный конвертер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Десятичное число " + decimalNumber + " равно восьмеричному числу " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Восьмеричное число " + octalNumber + " равно десятичному числу " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber){
+        if (decimalNumber > 0){
+            int octal = 0;
+            for (int i = 0; decimalNumber != 0; i++){
+                octal = octal + (decimalNumber % 8) * (int) Math.pow(10, i);
+                decimalNumber = decimalNumber / 8;
+            }
+            return octal;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static int toDecimal(int octalNumber) {
+        if (octalNumber > 0) {
+            int decimal = 0;
+            for (int i = 0; octalNumber != 0; i++) {
+                decimal = decimal + (octalNumber % 10) * (int) Math.pow(8, i);
+                octalNumber = octalNumber / 10;
+            }
+            return decimal;
+        } else {
+            return 0;
+        }
+    }
+}
+
+/* Публичный статический метод toBinary(int) должен переводить целое число, полученное в качестве входящего параметра, из десятичной системы счисления в двоичную и возвращать его строковое представление. А публичный статический метод toDecimal(String) наоборот — из строкового представления двоичного числа в десятичное число.
+Методы работают только с положительными числами и не пустыми строками. Если входящий параметр меньше или равен 0, метод toBinary(int) возвращает пустую строку. Если входящий параметр — пустая строка или null, то метод toDecimal(String) возвращает 0.
+Твоя задача — реализовать эти методы.
+
+Один из алгоритмов перевода десятичного числа в представление двоичного числа следующий:
+while(десятичное число не равно 0) {
+двоичное представление = остаток от деления десятичного числа на 2 + двоичное представление
+десятичное число = десятичное число / 2
+}
+
+Один из алгоритмов перевода представления двоичного числа в десятичное число следующий:
+for (int i = 0; i < длины двоичного представления; i++) {
+десятичное число = десятичное число + число из двоичного представления * на 2 в степени i
+}
+
+Изначально берется крайнее правое число из двоичного представления. С каждой итерацией цикла берется следующее число ближе к началу двоичного представления. */
+
+import java.util.regex.Pattern;
+
+/* 
+Двоичный конвертер
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int decimalNumber = Integer.MAX_VALUE;
+        System.out.println("Десятичное число " + decimalNumber + " равно двоичному числу " + toBinary(decimalNumber));
+        String binaryNumber = "1111111111111111111111111111111";
+        System.out.println("Двоичное число " + binaryNumber + " равно десятичному числу " + toDecimal(binaryNumber));
+    }
+
+    public static String toBinary(int decimalNumber) {
+        if (decimalNumber > 0) {
+            String binary = "";
+            while (decimalNumber != 0){
+                binary = (decimalNumber % 2) + binary;
+                decimalNumber = decimalNumber / 2;
+            }
+            return binary;
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        if ((binaryNumber != null) && !(binaryNumber.equals(""))){
+            int decimal = 0;
+            int[]mas = new int[binaryNumber.length()];
+            for (int i = 0; i < mas.length; i++){
+                mas[i] = Character.getNumericValue(binaryNumber.charAt(mas.length - 1 - i));
+            }
+            for (int i = 0; i < mas.length; i++){
+                decimal = decimal + mas[i] * (int) (Math.pow(2,i));
+            }
+            return decimal;
+        }
+        else {
+            return 0;
+        }
+    }
+}
