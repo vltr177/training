dc50eb6adbd5c5d4afc58f15ff8328dfe5eab4be
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-02-27 22:50:36 +0300

    Update 09_encodings.java

--- java-rush/09_encodings.java
+++ java-rush/09_encodings.java
@@ -123,3 +123,70 @@ public class Solution {
         }
     }
 }
+
+
+/* Публичный статический метод toHex(int) должен переводить целое число, полученное в качестве входящего параметра, из десятичной системы счисления в шестнадцатеричную и возвращать его строковое представление. А публичный статический метод toDecimal(String) наоборот — из строкового представления шестнадцатеричного числа в десятичное число.
+Методы работают только с положительными числами и не пустыми строками. Если входящий параметр меньше или равен 0, метод toHex(int) возвращает пустую строку. Если входящий параметр — пустая строка или null, то метод toDecimal(String) возвращает 0.
+Твоя задача реализовать эти методы.
+
+Также в классе Solution есть константа HEX, которая содержит в себе все символы, используемые в шестнадцатеричной системе.
+
+Один из алгоритмов перевода десятичного числа в шестнадцатеричное следующий:
+while(десятичное число не равно 0) {
+представление шестнадцатеричного числа = символ из строки HEX с индексом, равным остатку от деления десятичного числа на 16
++ представление шестнадцатеричного числа
+десятичное число = десятичное число / 16
+}
+
+Один из алгоритмов перевода шестнадцатеричного числа в десятичное следующий:
+for (int i = 0; i < длина входящей строки; i++) {
+десятичное число = 16 * десятичное число + индекс символа из строки HEX, равного символу из входящей строки с индексом i
+} /*
+
+import java.util.regex.Pattern;
+
+/* 
+Шестнадцатеричный конвертер
+*/
+
+public class Solution {
+    private static final String HEX = "0123456789abcdef";
+
+    public static void main(String[] args) {
+        int decimalNumber = 324234;
+        System.out.println("Десятичное число " + decimalNumber + " равно шестнадцатеричному числу " + toHex(decimalNumber));
+        String hexNumber = "4f28a";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно десятичному числу " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        if (decimalNumber >= 0){
+            String hex = "";
+            while (decimalNumber != 0){
+                hex = HEX.charAt(decimalNumber % 16) + hex;
+                decimalNumber = decimalNumber / 16;
+            }
+            return hex;
+        }
+        else {
+            return "";
+        }
+    }
+
+    public static int toDecimal(String hexNumber) {
+        if ((hexNumber != null) && !(hexNumber.equals(""))){
+            int decimal = 0;
+            char[] mas = new char[hexNumber.length()];
+            for (int i = 0; i < mas.length; i++){
+                mas[i] = hexNumber.charAt(i);
+            }
+            for (int i = 0; i < hexNumber.length(); i++){
+                decimal = 16 * decimal + HEX.indexOf(mas[i]);
+            }
+            return decimal;
+        }
+        else {
+            return 0;
+        }
+    }
+}
