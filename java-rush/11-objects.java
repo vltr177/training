b8b64bc1b16147496ff3c0c346b3b44f96efc6fa
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-03-02 12:20:00 +0300

    Update 11-objects.java

--- java-rush/11-objects.java
+++ java-rush/11-objects.java
@@ -108,3 +108,41 @@ public class Car {
         System.out.println(lamborghini.hashCode() == ferrari.hashCode());
     }
 }
+
+/* 
+У класса City есть два поля - название города и текущая температура воздуха. Также в классе Solution есть метод showWeather: он выводит в консоли информацию о погоде в городе, который передан в качестве аргумента метода. Реализуй метод showWeather, который должен выводить в консоли информацию в таком формате: В городе "название города" сегодня температура воздуха "температура в городе"
+В методе main один раз нужно вызвать метод showWeather с нужным аргументом. */
+
+public class City {
+    private String name;
+    private int temperature;
+
+    public City(String name, int temperature) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+}
+
+/* 
+Прогноз погоды
+*/
+
+public class Solution {
+
+    public static void showWeather(City city) {
+        System.out.println("В городе " + city.getName() + " сегодня температура воздуха " + city.getTemperature());
+    }
+
+    public static void main(String[] args) {
+        City city = new City("Дубай", 40);
+        showWeather(city);
+    }
+}
