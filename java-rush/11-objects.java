1cdcbd5b26f86da07a81a94a41324a8e95c08cd7
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-03-02 11:16:53 +0300

    Update 11-objects.java

--- java-rush/11-objects.java
+++ java-rush/11-objects.java
@@ -61,3 +61,50 @@ public class Iphone {
     }
 
 }
+
+/* Напиши свою реализацию hashCode, используя переменные model и year. Если эти поля у двух объектов одинаковые, то должен возвращаться одинаковый hashCode. Если правильно реализовать метод hashCode, вывод должен быть таким:
+true
+true
+true
+true
+false
+false */
+
+import java.util.Objects;
+
+/* 
+Создаем свой hashCode
+*/
+
+public class Car {
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    @Override
+    public int hashCode(){
+        int result = model == null? 0 : model.hashCode();
+        result = result + year;
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Car lamborghini = new Car("Lamborghini", 2020);
+        Car lamborghini1 = new Car("Lamborghini", 2020);
+        Car ferrari = new Car("Ferrari", 2020);
+        Car ferrari1 = new Car("Ferrari", 2020);
+        Car bugatti = new Car("Bugatti", 2020);
+        Car bugatti1 = new Car("Bugatti", 2020);
+        System.out.println(ferrari.hashCode() == ferrari.hashCode());
+        System.out.println(lamborghini.hashCode() == lamborghini1.hashCode());
+        System.out.println(ferrari.hashCode() == ferrari1.hashCode());
+        System.out.println(bugatti.hashCode() == bugatti1.hashCode());
+        System.out.println(bugatti.hashCode() == lamborghini.hashCode());
+        System.out.println(lamborghini.hashCode() == ferrari.hashCode());
+    }
+}
