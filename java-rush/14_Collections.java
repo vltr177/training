a6ce694c0539030ec3d835566df4ad9c09655ed9
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-03-13 16:47:58 +0500

    Update 14_Collections.java

--- java-rush/14_Collections.java
+++ java-rush/14_Collections.java
@@ -73,3 +73,72 @@ public class StringLinkedList {
         private Node next;
     }
 }
+
+
+/* Решая эту задачу, ты научишься извлекать элемент из двусвязного списка.
+Мы реализовали метод add, который добавляет элементы в конец списка. Тебе нужно реализовать метод get(int), который вернет строку под индексом, переданным в метод. Если строки с таким индексом нет, нужно вернуть null.
+Помни, что first и last не имеют значений, а ссылаются только на первый и последний элемент соответственно.
+
+Требования:
+•	Метод get(int index) должен возвращать элемент, который находится под индексом index в списке. */
+
+/* 
+StringLinkedList, часть 2
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        StringLinkedList stringLinkedList = new StringLinkedList();
+        stringLinkedList.add("1");
+        stringLinkedList.add("2");
+        stringLinkedList.add("3");
+        stringLinkedList.add("4");
+        stringLinkedList.add("5");
+
+        System.out.println(stringLinkedList.get(0)); // должно вывести "1"
+        System.out.println(stringLinkedList.get(2)); // должно вывести "3"
+        System.out.println(stringLinkedList.get(4)); // должно вывести "5"
+        System.out.println(stringLinkedList.get(5)); // должно вывести null
+    }
+}
+
+public class StringLinkedList {
+    private Node first = new Node();
+    private Node last = new Node();
+
+    public StringLinkedList() {
+        first.next = last;
+        last.prev = first;
+    }
+
+    public void add(String value) {
+        Node newNode = new Node();
+        newNode.value = value;
+
+        Node prevLast = last.prev;
+        prevLast.next = newNode;
+        newNode.prev = prevLast;
+        newNode.next = last;
+        last.prev = newNode;
+    }
+
+    public String get(int index) {
+        Node node = first.next;
+        int count = 0;
+        while ((node != null) && (node != last)){
+            if (count == index){
+                return node.value;
+            }
+            count ++;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public static class Node {
+        private Node prev;
+        private String value;
+        private Node next;
+    }
+}
