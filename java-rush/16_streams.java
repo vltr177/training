df3168e86757b849ab02ff42a9acfb58329a741d
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-03-19 20:30:16 +0300

    Update 16_streams.java

--- java-rush/16_streams.java
+++ java-rush/16_streams.java
@@ -61,3 +61,33 @@ public class Solution {
         }
     }
 }
+
+
+/* Напиши программу, которая считывает из консоли имя текстового файла, далее читает строки из этого файла (используй метод readAllLines(Path) класса Files) и выводит их на экран через одну, начиная с первой.
+
+Требования:
+•	Программа должна считать из консоли путь к файлу.
+•	Программа должна вывести в консоли содержимое файла согласно условию.
+•	Для чтения строк из файла нужно использовать метод readAllLines(Path) класса Files.
+•	Не используй следующие классы: File, FileInputStream, FileOutputStream, FileReader, FileWriter пакета java.io. */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Пропускаем не всех
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner console = new Scanner(System.in);
+        List<String> lines = Files.readAllLines(Paths.get(console.nextLine()));
+        for (int i = 0; i < lines.size(); i += 2)
+            System.out.println(lines.get(i));
+    }
+}
