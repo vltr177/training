764215b7e646dccdf8899e7fafebdb925c08e271
Vadim Barchuk <vadim.barchuk@gmail.com>
2025-04-15 20:36:23 +0300

    Create 20_games05_rocket.java

---
+++ java-rush/20_games05_rocket.java
@@ -0,0 +1,435 @@
+package com.javarush.games.moonlander;
+
+import com.javarush.engine.cell.*;
+import java.util.Arrays;
+
+public class Rocket extends GameObject {
+    private double speedY = 0;
+    private double speedX = 0;
+    private double boost = 0.05;
+    private double slowdown = boost / 10;
+    private RocketFire downFire;
+    private RocketFire leftFire;
+    private RocketFire rightFire;
+
+    public Rocket(double x, double y) {
+        super(x, y, ShapeMatrix.ROCKET);
+        downFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_DOWN_1, ShapeMatrix.FIRE_DOWN_2, ShapeMatrix.FIRE_DOWN_3));
+        leftFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_SIDE_1, ShapeMatrix.FIRE_SIDE_2));
+        rightFire = new RocketFire(Arrays.asList(ShapeMatrix.FIRE_SIDE_1, ShapeMatrix.FIRE_SIDE_2));
+    }
+
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed) {
+            speedY -= boost;
+        } else {
+            speedY += boost;
+        }
+        y += speedY;
+
+        if (isLeftPressed) {
+            speedX -= boost;
+            x += speedX;
+        } else if (isRightPressed) {
+            speedX += boost;
+            x += speedX;
+        } else if (speedX > slowdown) {
+            speedX -= slowdown;
+        } else if (speedX < -slowdown) {
+            speedX += slowdown;
+        } else {
+            speedX = 0;
+        }
+        x += speedX;
+        checkBorders();
+        switchFire(isUpPressed, isLeftPressed, isRightPressed);
+    }
+
+    private void checkBorders() {
+        if (x < 0) {
+            x = 0;
+            speedX = 0;
+        } else if (x + width > MoonLanderGame.WIDTH) {
+            x = MoonLanderGame.WIDTH - width;
+            speedX = 0;
+        }
+        if (y <= 0) {
+            y = 0;
+            speedY = 0;
+        }
+    }
+
+    public boolean isStopped() {
+        return (speedY < 10 * boost);
+    }
+
+    public boolean isCollision(GameObject object) {
+        int transparent = Color.NONE.ordinal();
+
+        for (int matrixX = 0; matrixX < width; matrixX++) {
+            for (int matrixY = 0; matrixY < height; matrixY++) {
+                int objectX = matrixX + (int) x - (int) object.x;
+                int objectY = matrixY + (int) y - (int) object.y;
+
+                if (objectX < 0 || objectX >= object.width || objectY < 0 || objectY >= object.height) {
+                    continue;
+                }
+
+                if (matrix[matrixY][matrixX] != transparent && object.matrix[objectY][objectX] != transparent) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void land(){
+        y -= 1;
+    }
+    
+    public void crash(){
+        matrix = ShapeMatrix.ROCKET_CRASH;
+    }
+    
+    private void switchFire(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed){
+        if (isUpPressed){
+            downFire.x = x + width/2;
+            downFire.y = y + height;
+            downFire.show();
+        }
+        else
+            downFire.hide();
+        if (isLeftPressed){
+            leftFire.x = x + width;
+            leftFire.y = y + height;
+            leftFire.show();
+        }
+        else
+            leftFire.hide();
+        if (isRightPressed){
+            rightFire.x = x - ShapeMatrix.FIRE_SIDE_1[0].length;
+            rightFire.y = y + height;
+            rightFire.show();
+        }
+        else
+            rightFire.hide();    
+    }
+    
+    @Override
+    public void draw(Game game){
+        super.draw(game);
+        downFire.draw(game);
+        leftFire.draw(game);
+        rightFire.draw(game);
+    }
+    
+}
+
+package com.javarush.games.moonlander;
+
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+    public double x;
+    public double y;
+    public int[][] matrix;
+    public int width;
+    public int height;
+
+    public GameObject(double x, double y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+        this.width = matrix[0].length;
+        this.height = matrix.length;
+    }
+
+    public void draw(Game game) {
+        if (matrix == null) {
+            return;
+        }
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int colorIndex = matrix[j][i];
+                game.setCellColor((int) x + i, (int) y + j, Color.values()[colorIndex]);
+            }
+        }
+    }
+}
+
+package com.javarush.games.moonlander;
+
+import java.util.List;
+import com.javarush.engine.cell.*;
+
+
+public class RocketFire extends GameObject{
+    
+    private List<int[][]> frames;
+    private int frameIndex;
+    private boolean isVisible;
+    
+    public RocketFire(List<int[][]> frameList){
+        super(0,0, frameList.get(0));
+        frames = frameList;
+        frameIndex = 0;
+        isVisible = false;
+    }
+    
+    private void nextFrame(){
+        frameIndex += 1;
+        if (frameIndex >= frames.size())
+            frameIndex = 0;
+        matrix = frames.get(frameIndex);
+    }
+    
+    @Override
+    public void draw(Game game){
+        if (isVisible == false)
+            return;
+        nextFrame();
+        super.draw(game);
+    }
+    
+    public void show(){
+        isVisible = true;
+    }
+    
+    public void hide(){
+        isVisible = false;
+    }
+}
+
+package com.javarush.games.moonlander;
+
+public class ShapeMatrix {
+    public static final int[][] ROCKET = new int[][]{
+            {0, 0, 0, 6, 0, 0, 0},
+            {0, 0, 6, 6, 6, 0, 0},
+            {0, 0, 6, 0, 6, 0, 0},
+            {0, 0, 6, 6, 6, 0, 0},
+            {0, 0, 6, 0, 6, 0, 0},
+            {0, 0, 6, 6, 6, 0, 0},
+            {0, 6, 6, 0, 6, 6, 0},
+            {6, 6, 6, 6, 6, 6, 6},
+            {6, 0, 0, 6, 0, 0, 6},
+    };
+
+    public static final int[][] ROCKET_CRASH = new int[][]{
+            {0, 0, 0, 5, 0, 0, 0},
+            {0, 0, 5, 5, 5, 0, 0},
+            {0, 0, 5, 0, 5, 0, 0},
+            {0, 0, 5, 5, 5, 0, 0},
+            {0, 0, 5, 0, 5, 0, 0},
+            {0, 0, 5, 5, 5, 0, 0},
+            {0, 5, 5, 0, 5, 5, 0},
+            {5, 5, 5, 5, 5, 5, 5},
+            {5, 0, 0, 5, 0, 0, 5},
+    };
+
+    public static final int[][] LANDSCAPE = new int[][]{
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0},
+            {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+            {0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+
+    public static final int[][] PLATFORM = new int[][]{
+            {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
+    };
+
+    public static final int[][] FIRE_DOWN_1 = new int[][]{
+            {5},
+            {5},
+            {0},
+            {5},
+            {5},
+            {0},
+            {5},
+    };
+
+    public static final int[][] FIRE_DOWN_2 = new int[][]{
+            {0},
+            {5},
+            {5},
+            {0},
+            {5},
+            {5},
+            {0},
+    };
+
+    public static final int[][] FIRE_DOWN_3 = new int[][]{
+            {5},
+            {0},
+            {5},
+            {5},
+            {0},
+            {5},
+            {5},
+    };
+
+    public static final int[][] FIRE_SIDE_1 = new int[][]{
+            {5, 0, 5}
+    };
+
+    public static final int[][] FIRE_SIDE_2 = new int[][]{
+            {0, 5, 0}
+    };
+}
+
+package com.javarush.games.moonlander;
+
+import com.javarush.engine.cell.*;
+
+public class MoonLanderGame extends Game {
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
+    private Rocket rocket;
+    private GameObject landscape;
+    private boolean isUpPressed;
+    private boolean isLeftPressed;
+    private boolean isRightPressed;
+    private GameObject platform;
+    private boolean isGameStopped;
+    private int score;
+    
+    
+    @Override
+    public void initialize(){
+        showGrid(false);
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+    
+    @Override
+    public void onTurn(int step){
+        rocket.move(isUpPressed, isLeftPressed, isRightPressed);
+        check();
+        if (score > 0)
+            score -= 1;
+        setScore(score);
+        drawScene();
+    }
+    
+    @Override
+    public void setCellColor(int x, int y, Color color){
+        if ( (x < 0) || (x >= WIDTH) || (y < 0) || (y >= WIDTH) )
+            return;
+        super.setCellColor (x, y, color);
+    }
+    
+    @Override
+    public void onKeyPress(Key key){
+        if ((key == Key.SPACE) && isGameStopped){
+            createGame();
+            return;
+        }
+        if (key == Key.UP)
+            isUpPressed = true;
+        if (key == Key.LEFT){
+            isLeftPressed = true;
+            isRightPressed = false;
+        }
+        if (key == Key.RIGHT){
+            isLeftPressed = false;
+            isRightPressed = true;
+        }
+    }
+    
+    @Override
+    public void onKeyReleased(Key key){
+        if (key == Key.UP)
+            isUpPressed = false;
+        if (key == Key.LEFT){
+            isLeftPressed = false;
+        }
+        if (key == Key.RIGHT)
+            isRightPressed = false;
+    }
+    
+    private void createGame(){
+        isUpPressed = false;
+        isLeftPressed = false;
+        isRightPressed = false;
+        isGameStopped = false;
+        createGameObjects();
+        drawScene();
+        setTurnTimer(50);
+        score = 1000;
+        
+    }
+    
+    private void drawScene(){
+        for (int i = 0; i < WIDTH; i++)
+            for (int j = 0; j < HEIGHT; j++)
+                setCellColor(i, j, Color.BLACK);
+        rocket.draw(this);
+        landscape.draw(this);
+    }
+    
+    private void createGameObjects(){
+        rocket = new Rocket(WIDTH/2, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+        platform = new GameObject(23, MoonLanderGame.HEIGHT-1, ShapeMatrix.PLATFORM);
+    }
+    
+    private void check(){
+        if (rocket.isCollision(landscape) || (!rocket.isCollision(platform) && rocket.isStopped()))
+            gameOver();
+        if (rocket.isCollision(platform) && rocket.isStopped()) 
+            win();
+    }
+    
+    private void win(){
+        rocket.land();
+        isGameStopped = true;
+        showMessageDialog(Color.WHITE, "КРАСАВЧЕГ", Color.GREEN, 80);
+        stopTurnTimer();
+        
+    }
+    
+    private void gameOver(){
+        score = 0;
+        rocket.crash();
+        isGameStopped = true;
+        showMessageDialog(Color.WHITE, "ЛУЗЕР", Color.RED, 80);
+        stopTurnTimer();
+    }
+}
